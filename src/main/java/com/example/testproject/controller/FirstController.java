37e463fb62ce64549fee801b301767dff86d71eb
sumineda <148422747+sumineda@users.noreply.github.com>
2023-11-02 14:28:55 +0900

    회원가입 페이지 테스트
    테스트해보기

--- src/main/java/com/example/testproject/controller/FirstController.java
+++ src/main/java/com/example/testproject/controller/FirstController.java
@@ -1,13 +1,31 @@
 package com.example.testproject.controller;
 
+import com.example.testproject.dto.ArticleForm;
+import com.example.testproject.entity.Article;
+import com.example.testproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FirstController {
-//    main이 주석은 지우기
-    @GetMapping("/articles/Main")
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/articles/join")
     public String niceToMeetYou(){
-        return "/articles/Main_page";
+        return "/articles/join";
+    }
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        //1.dto를 entity로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+        //2. repository로 엔티티를 디비에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+        return "";
     }
 }
