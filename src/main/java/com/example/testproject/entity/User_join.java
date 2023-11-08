782e071656554887379e1789342e69a98b7a619c
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-11-08 10:08:57 +0900

    User_join 추가
    Controller, dto, entity, repository

---
+++ src/main/java/com/example/testproject/entity/User_join.java
@@ -0,0 +1,33 @@
+package com.example.testproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+//=========== 회원가입 엔티티입니다 ==========
+@ToString
+@AllArgsConstructor
+@Entity
+public class User_join {
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 id 생성
+    private Long i_d;
+
+    @Column(name = "users_id")
+    private String id;
+
+    @Column(name = "users_pw")
+    private String pw;
+
+    @Column(name = "users_pw_check")
+    private String pw_check;
+
+    @Column(name = "users_name")
+    private String name;
+
+    @Column(name = "users_phone")
+    private String phone;
+
+    @Column(name = "users_email")
+    private String email;
+}
+
