3cc09ad3400b37d22e26bfb3169909c45c658538
sumineda <148422747+sumineda@users.noreply.github.com>
2023-11-08 17:19:41 +0900

    로그 리네임함
    로그 info가 인식이 안돼서 새로 만듦

---
+++ src/main/java/com/example/testproject/entity/User_login.java
@@ -0,0 +1,20 @@
+package com.example.testproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Entity
+public class User_login {
+    @jakarta.persistence.Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 id 생성
+    private Long i_d;
+
+    @Column(name = "users_id")
+    private String Id;
+
+    @Column(name = "users_pw")
+    private String Pw;
+}
---
+++ src/main/java/com/example/testproject/repository/User_loginRepository.java
@@ -0,0 +1,7 @@
+package com.example.testproject.repository;
+
+import com.example.testproject.entity.User_login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface User_loginRepository extends JpaRepository<User_login, Long> {
+}
