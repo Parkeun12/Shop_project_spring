fc7484a4d4b782a952335df722520581ca062f19
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-11-08 10:08:28 +0900

    필요없는 파일 삭제
    Article관련 컨트롤러, dto, entity, repository 삭제
    person관련 컨트롤러, dto, entity, repository 삭제

--- src/main/java/com/example/testproject/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.testproject.repository;
-
-import com.example.testproject.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//================ 회원가입 리포지토리입니다 ==============
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- src/main/java/com/example/testproject/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.testproject.repository;
-
-import com.example.testproject.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
