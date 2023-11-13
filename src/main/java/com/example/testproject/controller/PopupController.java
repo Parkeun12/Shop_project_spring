0c9f3a50cf7f7bed030480e37c1e99bd84504403
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-11-13 17:58:37 +0900

    Main화면 db받아오기
    바로구매 해결중

---
+++ src/main/java/com/example/testproject/controller/PopupController.java
@@ -0,0 +1,38 @@
+package com.example.testproject.controller;
+
+import com.example.testproject.entity.Product;
+import com.example.testproject.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class PopupController {
+
+    @Autowired
+    public ProductRepository productRepository;
+
+    @GetMapping("/mainshop/payment_popup/{productNum}")
+    public String MainShop_popup1(@PathVariable Long productNum, Model model){
+
+        log.info("productNum = " + productNum);
+
+        Product productEntity = productRepository.findById(productNum).orElse(null);
+
+        model.addAttribute("paymentPopup", productEntity);
+
+        return "articles/payment_popup";
+    }
+
+    @GetMapping("/mainshop/zzim_popup")
+    public String MainShop_popup2(){
+        return "articles/zzimPopup";
+    }
+}
