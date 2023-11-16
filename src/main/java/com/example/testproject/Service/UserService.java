01af5f0aaa0132ba3390ce667d0daecac51e676e
sumineda <148422747+sumineda@users.noreply.github.com>
2023-11-16 16:23:44 +0900

    유저 서비스 추가

--- src/main/java/com/example/testproject/Service/UserService.java
+++ src/main/java/com/example/testproject/Service/UserService.java
@@ -1,4 +1,23 @@
 package com.example.testproject.Service;
 
+import com.example.testproject.entity.Users;
+import com.example.testproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Users create(String userId, String pw) {
+        Users user = new Users();
+        user.setUserId(userId);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPw(passwordEncoder.encode(pw));
+        this.userRepository.save(user);
+        return user;
+    }
 }
