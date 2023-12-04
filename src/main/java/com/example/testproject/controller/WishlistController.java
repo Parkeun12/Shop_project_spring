69bf88d34b169ba0e6ee3d3da6f7ad933f3cc4e4
Echae <91455648+Parkeun12@users.noreply.github.com>
2023-12-04 10:52:31 +0900

    삭제부분 수정

--- src/main/java/com/example/testproject/controller/WishlistController.java
+++ src/main/java/com/example/testproject/controller/WishlistController.java
@@ -84,29 +84,44 @@ public class WishlistController {
     }
 
 //     관심상품 삭제 기능
-    @GetMapping("/wishlist/{wishlistId}/delete")
-    public String deleteWishlistItem(@PathVariable Long wishlistId) {
+//    @GetMapping("/wishlist/{productNum}/delete")
+//    public String deleteWishlistItem(@PathVariable Long productNum) {
+//
+//        // WishlistRepository에서 Wishlist를 가져오기
+//        Optional<Wishlist> wishlistOptional = wishlistRepository.findById(wishlistId);
+//
+//        // Wishlist가 존재하는지 확인
+//        if (wishlistOptional.isPresent()) {
+//            Wishlist wishlist = wishlistOptional.get();
+//
+//            // Wishlist에서 상품 정보 가져오기 //필요한가?
+//            Product product = wishlist.getProduct();
+//
+//            // Wishlist에서 상품 삭제
+//            wishlistRepository.deleteById(wishlistId);
+//
+//            // 상품 삭제 후 관심상품 페이지로 리다이렉트
+//            return "redirect:/wishlist/" + wishlist.getUsers().getId();
+//        } else {
+//            // Wishlist가 존재하지 않을 경우
+//            return "redirect:/mainshop";
+//        }
+//    }
 
-        // WishlistRepository에서 Wishlist를 가져오기
-        Optional<Wishlist> wishlistOptional = wishlistRepository.findById(wishlistId);
+    @GetMapping("/wishlist/{productNum}/delete")
+    public String deleteWishlistItem(@PathVariable Long productNum) {
 
-        // Wishlist가 존재하는지 확인
-        if (wishlistOptional.isPresent()) {
-            Wishlist wishlist = wishlistOptional.get();
+        // 현재 로그인한 사용자의 정보 가져오기
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserId = authentication.getName();
 
-            // Wishlist에서 상품 정보 가져오기 //필요한가?
-            Product product = wishlist.getProduct();
+        // 현재 로그인한 사용자의 Wishlist에서 productNum에 해당하는 항목 삭제
+        wishlistRepository.deleteByUsersUsernameAndProductProductNum(currentUserId, productNum);
 
-            // Wishlist에서 상품 삭제
-            wishlistRepository.deleteById(wishlistId);
-
-            // 상품 삭제 후 관심상품 페이지로 리다이렉트
-            return "redirect:/wishlist/" + wishlist.getUsers().getId();
-        } else {
-            // Wishlist가 존재하지 않을 경우
-            return "redirect:/mainshop";
-        }
+        // 상품 삭제 후 관심상품 페이지로 리다이렉트
+        return "redirect:/wishlist/" + currentUserId;
     }
+
 
     // delete 내 현재 사용자가 선택한 상품 찾기 추후 리스트부분 수정 보완되면 마찬가지로 수정 필요
 
--- src/main/java/com/example/testproject/repository/WishlistRepository.java
+++ src/main/java/com/example/testproject/repository/WishlistRepository.java
@@ -4,10 +4,12 @@ package com.example.testproject.repository;
 import com.example.testproject.entity.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
     boolean existsByUsersUsernameAndProductProductNum(String username, Long productNum);
 
-//    Optional<Wishlist> findByUsersUsernameAndProductNum(String username, String productNum);
+    Optional<Wishlist> deleteByUsersUsernameAndProductProductNum(String username, Long productNum);
 
 
 }
